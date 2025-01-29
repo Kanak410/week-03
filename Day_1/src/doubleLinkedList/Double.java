526d01f52c7a2cd0045e31128f115c1e1bf737f5
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-29 10:22:40 +0530

    added movie management file

---
+++ Day_1/src/doubleLinkedList/Double.java
@@ -0,0 +1,4 @@
+package doubleLinkedList;
+
+public class Double {
+}
---
+++ Day_1/src/doubleLinkedList/movieManagement/Main.java
@@ -0,0 +1,29 @@
+package doubleLinkedList.movieManagement;
+
+public class Main {
+    public static void main(String[] args) {
+        doubleLinkedList movieList = new doubleLinkedList();
+
+        // Add movies
+        movieList.addAtEnd("Inception", "Christopher Nolan", 2010, 8.8);
+        movieList.addAtEnd("The Dark Knight", "Christopher Nolan", 2008, 9.0);
+        movieList.addAtFront("Interstellar", "Christopher Nolan", 2014, 8.6);
+
+        // Display movies
+        movieList.displayForward();
+        System.out.println();
+        movieList.displayReverse();
+
+        // Search for a movie
+        movieList.searchByDirectorOrRating("Christopher Nolan", 9.0);
+
+        // Update rating
+        movieList.updateRating("Inception", 9.0);
+
+        // Remove a movie
+        movieList.removeByTitle("The Dark Knight");
+
+        // Display movies after removal
+        movieList.displayForward();
+    }
+}
