0367b2603f8126b986443bd174c5f2263f54584a
Kanak Rajput <rajputkanak078@gmail.com>
2025-01-31 10:27:48 +0530

    Added src/hashMap/pairsum/PairWithGivenSum.java

---
+++ Day_2/src/hashMap/pairsum/PairWithGivenSum.java
@@ -0,0 +1,23 @@
+package hashMap.pairsum;
+import java.util.*;
+
+public class PairWithGivenSum {
+    public static boolean hasPairWithSum(int[] arr, int target) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            if (seen.contains(target - num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 6, 8, 10};
+        int target = 14;
+        System.out.println("Pair with given sum exists: " + hasPairWithSum(arr, target));
+    }
+}
+
