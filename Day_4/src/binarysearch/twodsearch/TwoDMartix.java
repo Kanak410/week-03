2ceef839c3c3d1a77f7bbfcbb84e8b9aa356fe77
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-05 15:52:05 +0530

    [kanak rajput]added file of 2-D array

---
+++ Day_4/src/binarysearch/twodsearch/TwoDMartix.java
@@ -0,0 +1,42 @@
+package binarysearch.twodsearch;
+
+public class TwoDMartix {
+
+        public static boolean searchMatrix(int[][] matrix, int target) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return false;
+            }
+
+            int rows = matrix.length;
+            int cols = matrix[0].length;
+            int left = 0, right = rows * cols - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                int midValue = matrix[mid / cols][mid % cols]; // Convert 1D index to 2D
+
+                if (midValue == target) {
+                    return true;
+                } else if (midValue < target) {
+                    left = mid + 1; // Search right half
+                } else {
+                    right = mid - 1; // Search left half
+                }
+            }
+
+            return false; // Target not found
+        }
+
+        public static void main(String[] args) {
+            int[][] matrix = {
+                    {1, 3, 5, 7},
+                    {10, 11, 16, 20},
+                    {23, 30, 34, 60}
+            };
+            int target = 3;
+            boolean result = searchMatrix(matrix, target);
+            System.out.println("Target " + target + " found: " + result);
+        }
+    
+
+}
